613e5e998b9c57f51b0a19fdf237fb64769ed0c6
Shawn N <79764274+ShawnN24@users.noreply.github.com>
2024-01-05 01:43:59 -0700

    Carousel completed

--- src/script.js
+++ src/script.js
@@ -1,8 +1,1 @@
 //comment
-const carouselHome = document.getElementById('carouselHome');
-
-carouselHome.addEventListener('carousel-control-next-icon', event => {
-    onclick
-})
-
-const carouselNext = document.getElementById('')
