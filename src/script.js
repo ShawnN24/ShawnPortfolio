b52f625456d3866ce5817ae7a97e5dbeb85c6b59
Shawn N <79764274+ShawnN24@users.noreply.github.com>
2024-01-04 18:26:39 -0700

    Home page fixes and Carousel pain 1/4/2024

---
+++ src/script.js
@@ -0,0 +1,8 @@
+//comment
+const carouselHome = document.getElementById('carouselHome');
+
+carouselHome.addEventListener('carousel-control-next-icon', event => {
+    onclick
+})
+
+const carouselNext = document.getElementById('')
