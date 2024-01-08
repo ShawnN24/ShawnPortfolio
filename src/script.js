f805a3eb72d0a0ccd786c5f721b66876caa39934
Shawn N <79764274+ShawnN24@users.noreply.github.com>
2024-01-08 10:30:38 -0700

    contact page in progress, formatting finished

--- src/script.js
+++ src/script.js
@@ -1,1 +1,12 @@
 //comment
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            document.querySelectorAll(".animated")[0].classList.add("fadeInLeft");
+            document.querySelectorAll(".animated")[1].classList.add("fadeInRight");
+            document.querySelectorAll(".animated")[2].classList.add("fadeInLeft");
+        }
+    })
+})
+
+observer.observe(document.querySelector(".container"));
